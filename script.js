5a7a9b09c45ed17cbe9a8555270f43153f28ec1f
aya yousef <aya.yousef.mahmoud@gmail.com>
2022-11-23 15:10:15 +0200

    intinal

---
+++ script.js
@@ -0,0 +1,27 @@
+var proDuct =document.querySelectorAll(".pro");
+var nameProduct =document.querySelector(".name-product");
+var btn =document.querySelector(".normal");
+var remove =document.querySelector(".remove")
+var purchasePrice =document.querySelector(".pro-price");
+var totalprice=0;
+proDuct.forEach(function(item){
+    item.onclick=function() {
+        totalprice += parseInt(item.getAttribute("price"))
+        nameProduct.innerHTML += item.getAttribute("name") + ","
+        nameProduct.style.display="block"
+    }
+    remove.onclick= function(){
+        nameProduct.innerHTML = ""
+        nameProduct.style.display="none"
+        remove.style.backgroundColor="#003028"
+        purchasePrice.style.display="none"
+        remove.style.border="none"
+
+    }
+})
+btn.onclick= function(){
+    purchasePrice.style.display="block"
+    btn.style.backgroundColor="#003028"
+    btn.style.border="none"
+    purchasePrice.innerHTML = "$"+(totalprice)
+}
